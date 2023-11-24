d802781115b0ec57c815513f79ad1b5e80901980
ivan <ix.pukhov@gmail.com>
2023-11-25 04:34:25 +0600

    f

--- service-worker.js
+++ service-worker.js
@@ -1,82 +1,74 @@
 const CACHE_NAME = 'ai-news-cache-v1';
-const URLS_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/index.js',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
+const STATIC_CACHE_URLS = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/index.js',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
 ];
 
-// Установка Service Worker и кеширование ресурсов
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Открыт кеш:', cache);
-        return cache.addAll(URLS_TO_CACHE);
-      })
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Открыт кеш:', cache);
+                return cache.addAll(STATIC_CACHE_URLS);
+            })
+    );
 });
 
-// Активация Service Worker и очистка старого кеша
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.filter(cacheName => cacheName !== CACHE_NAME)
-          .map(cacheName => caches.delete(cacheName))
-      );
-    })
-  );
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.filter(cacheName => cacheName !== CACHE_NAME)
+                    .map(cacheName => caches.delete(cacheName))
+            );
+        })
+    );
 });
 
-// Перехват сетевых запросов
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Возвращает кешированный ответ, если он есть, или выполняет запрос к сети
-        return response || fetch(event.request).then(fetchResponse => {
-          return caches.open(CACHE_NAME).then(cache => {
-            cache.put(event.request, fetchResponse.clone());
-            return fetchResponse;
-          });
-        });
-      })
-  );
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request).then(fetchResponse => {
+                    if (event.request.url.startsWith('/api/news')) {
+                        return caches.open(CACHE_NAME).then(cache => {
+                            cache.put(event.request, fetchResponse.clone());
+                            return fetchResponse;
+                        });
+                    } else {
+                        return fetchResponse;
+                    }
+                });
+            })
+    );
 });
 
-// Обработка сообщений от основного потока
 self.addEventListener('message', event => {
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.ready.then(registration => {
-            registration.active.postMessage({ type: 'get-cached-news' });
-        });
+    if (event.data && event.data.type === 'get-cached-news') {
+        serveCachedNews(event);
+        navigator.serviceWorker.controller.postMessage({ type: 'get-cached-news' });
     }
-
 });
 
-
-
-
-
-// Функция для предоставления кешированных новостей
 function serveCachedNews(event) {
-  const newsApiUrl = '/api/news'; // URL  API для новостей
-  caches.match(newsApiUrl)
-    .then(response => {
-      if (response) {
-        return response.json();
-      }
-      return null;
-    })
-    .then(data => {
-      if (data) {
-        event.source.postMessage({ type: 'cached-news', articles: data.items });
-      } else {
-        event.source.postMessage({ type: 'cached-news', articles: [] });
-      }
-    })
-    .catch(error => console.error('Ошибка при получении кешированных новостей:', error));
+    const newsApiUrl = '/api/news';
+    caches.match(newsApiUrl)
+        .then(response => {
+            if (response) {
+                return response.json();
+            }
+            return null;
+        })
+        .then(data => {
+            if (data) {
+                event.source.postMessage({ type: 'cached-news', articles: data.items });
+            } else {
+                event.source.postMessage({ type: 'cached-news', articles: [] });
+            }
+        })
+        .catch(error => console.error('Ошибка при получении кешированных новостей:', error));
 }
